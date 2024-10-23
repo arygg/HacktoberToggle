9cfabba9223a50dc2f20be4e8cddab4a1dfc6179
Asbjørn Rygg <asbjorn.rygg@if.no>
2024-10-23 10:08:52 +0200

    Initial commit

---
+++ src/Api/ApiRoutes.cs
@@ -0,0 +1,27 @@
+﻿namespace Api;
+
+public static class ApiRoutes
+{
+    public static void AddApiEndpoints(this WebApplication app)
+    {
+        var apiGroup = app.MapGroup("/api");
+
+        apiGroup.MapGet("/weatherforecast", (HttpContext httpContext) => { return GetWeatherForecasts(); })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+    }
+
+    static readonly string[] _summaries = { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+    private static WeatherForecast[] GetWeatherForecasts()
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+                })
+            .ToArray();
+        return forecast;
+    }
+}
